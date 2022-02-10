a063943ff257739b725a52fefe4b21d11df2a67b
Lilia Neftali Gastelum Diaz <97011520+lilia-gastelum@users.noreply.github.com>
2022-02-10 14:15:33 -0700

    Update src/utils/pipes/cash.js
    Co-authored-by: PacoMojica <pacomojica@hotmail.com>

--- src/utils/pipes/cash.js
+++ src/utils/pipes/cash.js
@@ -9,12 +9,14 @@ const cash = (text) => {
 
   const amount = parseFloat(text.toString());
   const cashFormatNumber = amount.toFixed(2);
-  const cashArray = cashFormatNumber.split(".");
-
-  const val = `${
-    cashArray[0].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") +
-    (cashArray[1] ? "." + cashArray[1] : "")
-  }`;
+  const [wholePart, decimalPart] = cashFormatNumber.split(".");
+  // the name of this variable should be based on the pattern
+  // that the regrex matches
+  // it seems to be matching groups of 3 digits
+  const REGEX = /\B(?=(\d{3})+(?!\d))/g;
+  const formattedWholePart = wholePart.toString().replace(REGEX, ",")
+  const formattedDecimalPart = decimalPart ? `.${decimalPart}` : "";
+  const value = formattedWholePart + decimalPart;
   return "$" + val;
 };
 
